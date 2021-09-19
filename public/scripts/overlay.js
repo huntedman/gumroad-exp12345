a6533be29ab6011c2dc7e6bf4b438d2e0b7fd474
huntedman <toomashaide@gmail.com>
2021-09-19 13:37:22 +0200

    Created and styled the form to submit a review.

--- public/scripts/overlay.js
+++ public/scripts/overlay.js
@@ -5,14 +5,16 @@ const toggleOverlay = (button) => {
   let $overlay = document.querySelector('#overlay');
 
   // Add event listener to button
-  $button.addEventListener('click', () => {
+  $button.addEventListener('click', (event) => {
+    if (!event.target.matches('#add_review_button')) return;
     state.closed ? (state.closed = false) : (state.closed = true);
     $overlay.classList.toggle('visible');
     $overlay.classList.toggle('invisible');
   });
 
   // Add event listener to overlay
-  $overlay.addEventListener('click', () => {
+  $overlay.addEventListener('click', (event) => {
+    if (!event.target.matches('#overlay')) return;
     state.closed ? (state.closed = false) : (state.closed = true);
     $overlay.classList.toggle('visible');
     $overlay.classList.toggle('invisible');
