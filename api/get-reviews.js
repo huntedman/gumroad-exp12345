ba423f965b9cfa1be376706a396d8fba21c036ee
huntedman <toomashaide@gmail.com>
2021-09-19 20:17:21 +0200

    Added endpoint to get all reviews

--- api/get-reviews.js
+++ api/get-reviews.js
@@ -1,11 +1,17 @@
+const AWS = require('aws-sdk');
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const { TABLE_NAME } = process.env;
+
 module.exports.main = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: 'Done!',
-    }),
-  };
+  const result = await docClient
+    .scan({
+      TableName: TABLE_NAME,
+    })
+    .promise();
+
+  console.log('Wata', result);
+
+  return result.Items;
 };
