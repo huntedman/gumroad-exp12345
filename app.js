3a90286434d13d0f3100ba3ab6a01338303a1463
huntedman <toomashaide@gmail.com>
2021-09-19 10:55:26 +0200

    Creating the basic html structure.
    Also setup livereload.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
 const app = express();
+
+app.use(connectLiveReload());
 
 app.use(express.static('public'));
 
